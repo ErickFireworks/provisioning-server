3f19c4f2fa334c557ededc21077d8afad43bdaa9
ErickFireworks <erick@fireworks.studio>
2023-04-24 12:11:09 -0600

    first commit

---
+++ server_provisioning.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#docker run -it -d --name aprov2 ubuntu
+#docker run -it -d -p 3000:80 --name apro_tes -v $(pwd)://root ubuntu
+#docker exec -it aprov2 bash
+
+#echo "hola"
+#sudo passwd
+#apt-get update
+#apt-get install nano
+#apt install nginx
+#service nginx start
+#Install python
+#apt install software-properties-common
+#add-apt-repository ppa:deadsnakes/ppa
+#apt update
+#apt install python3
+#Install maria db
+#apt update
+#apt install mariadb-server
+#systemctl start mariadb.service
+#apt install mariadb-server mariadb-client -y
+#systemctl enable mariadb.service
+#sudo mysql_secure_installation
+
