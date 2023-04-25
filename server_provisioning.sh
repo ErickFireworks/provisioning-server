a40a8dd5fb87302486692cda568a0407fde205a3
ErickFireworks <95870582+ErickFireworks@users.noreply.github.com>
2023-04-25 17:50:23 -0600

    Update server_provisioning.sh

--- server_provisioning.sh
+++ server_provisioning.sh
@@ -3,22 +3,86 @@
 #docker run -it -d -p 3000:80 --name apro_tes -v $(pwd)://root ubuntu
 #docker exec -it aprov2 bash
 
-#echo "hola"
+echo "hola"
+#Change pass root
+#sudo -i
 #sudo passwd
-#apt-get update
+
+#Firts step
+#sudo apt-get update
+
+#Install nano
 #apt-get install nano
-#apt install nginx
-#service nginx start
+
+#Install git
+#sudo apt install git-all
+
+#Install Nginx
+#sudo apt update
+#sudo apt install nginx
+#sudo service nginx start
+
 #Install python
-#apt install software-properties-common
-#add-apt-repository ppa:deadsnakes/ppa
-#apt update
-#apt install python3
-#Install maria db
-#apt update
-#apt install mariadb-server
+#sudo apt update
+#sudo apt install software-properties-common
+#sudo add-apt-repository ppa:deadsnakes/ppa
+#sudo apt update
+#sudo apt install python3
+
+#sudo apt install mariadb-server
 #systemctl start mariadb.service
-#apt install mariadb-server mariadb-client -y
-#systemctl enable mariadb.service
+
+#Install maria db
+#sudo apt update
+#sudo apt install mariadb-server mariadb-client -y
+#sudo systemctl enable mariadb.service
 #sudo mysql_secure_installation
+
+#sudo apt install nodejs -y
+#sudo apt install npm -y
+#nodejs --version
+#npm --version
+
+#Install Curl
+#sudo apt update
+#sudo apt install curl
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+
+#Install Node nvm
+#*nvm -v
+#*nvm ls-remote
+#nvm install node
+
+#Install php
+#sudo apt update
+#sudo apt show php -a
+#sudo apt install php
+#sudo apt install software-properties-common
+#sudo add-apt-repository ppa:ondrej/php
+#sudo apt-get update
+
+#For Nginx Web Server
+#sudo apt update   
+# sudo apt install php7.0-fpm   
+# sudo apt install php7.1-fpm   
+# sudo apt install php7.2-fpm   
+# sudo apt install php7.3-fpm   
+# sudo apt install php7.4-fpm   
+# sudo apt install php8.0-fpm 
+
+#sudo apt update
+# sudo apt install php7.0 
+# sudo apt install php7.1
+# sudo apt install php7.2
+# sudo apt install php7.3 
+# sudo apt install php7.4
+# sudo apt install php8.0
+
+#sudo apt update
+# sudo apt install php7.0-cli php7.0-xml php7.0-mysql 
+# sudo apt install php7.1-cli php7.1-xml php7.1-mysql
+# sudo apt install php7.2-cli php7.2-xml php7.2-mysql 
+# sudo apt install php7.3-cli php7.3-xml php7.3-mysql 
+# sudo apt install php7.3-cli php7.4-xml php7.4-mysql  
+# sudo apt install php7.3-cli php8.0-xml php8.0-mysql 
 
