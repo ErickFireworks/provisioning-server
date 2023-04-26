11429243fedba72eab7e720033c5e01d487c116d
ErickFireworks <erick@fireworks.studio>
2023-04-25 18:43:17 -0600

    Add service nginx In this commit fix service http default.

--- server_provisioning.sh
+++ server_provisioning.sh
@@ -9,7 +9,7 @@ echo "hola"
 #sudo passwd
 
 #Firts step
-#sudo apt-get update
+sudo apt-get update
 
 #Install nano
 #apt-get install nano
@@ -19,23 +19,23 @@ echo "hola"
 
 #Install Nginx
 #sudo apt update
-#sudo apt install nginx
-#sudo service nginx start
+sudo apt install nginx
+sudo service nginx start
 
 #Install python
-#sudo apt update
-#sudo apt install software-properties-common
-#sudo add-apt-repository ppa:deadsnakes/ppa
-#sudo apt update
-#sudo apt install python3
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3
 
 #sudo apt install mariadb-server
 #systemctl start mariadb.service
 
 #Install maria db
-#sudo apt update
-#sudo apt install mariadb-server mariadb-client -y
-#sudo systemctl enable mariadb.service
+sudo apt update
+sudo apt install mariadb-server mariadb-client -y
+sudo systemctl enable mariadb.service
 #sudo mysql_secure_installation
 
 #sudo apt install nodejs -y
@@ -44,25 +44,25 @@ echo "hola"
 #npm --version
 
 #Install Curl
-#sudo apt update
-#sudo apt install curl
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+sudo apt update
+sudo apt install curl
 
 #Install Node nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 #*nvm -v
 #*nvm ls-remote
 #nvm install node
 
 #Install php
-#sudo apt update
-#sudo apt show php -a
-#sudo apt install php
-#sudo apt install software-properties-common
-#sudo add-apt-repository ppa:ondrej/php
-#sudo apt-get update
+sudo apt update
+sudo apt show php -a
+sudo apt install php
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
 
 #For Nginx Web Server
-#sudo apt update   
+# sudo apt update   
 # sudo apt install php7.0-fpm   
 # sudo apt install php7.1-fpm   
 # sudo apt install php7.2-fpm   
@@ -71,12 +71,12 @@ echo "hola"
 # sudo apt install php8.0-fpm 
 
 #sudo apt update
-# sudo apt install php7.0 
-# sudo apt install php7.1
-# sudo apt install php7.2
-# sudo apt install php7.3 
-# sudo apt install php7.4
-# sudo apt install php8.0
+#sudo apt install php7.0 
+#sudo apt install php7.1
+#sudo apt install php7.2
+#sudo apt install php7.3 
+sudo apt install php7.4
+#sudo apt install php8.0
 
 #sudo apt update
 # sudo apt install php7.0-cli php7.0-xml php7.0-mysql 
@@ -86,3 +86,13 @@ echo "hola"
 # sudo apt install php7.3-cli php7.4-xml php7.4-mysql  
 # sudo apt install php7.3-cli php8.0-xml php8.0-mysql 
 
+#sudo service apache2 stop
+#sudo service nginx stop
+#sudo service nginx start
+#cd /var/www/html/
+#mv index.html index-old.html
+#sudo mv index.nginx-debian.html index.html
+#sudo service nginx restart
+
+#Create .bash_profile
+#Reboot system
